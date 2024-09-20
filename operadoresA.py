9d7fa749670199d3963d501359828ce550e19adb
CesarDanielOC <146128780+CesarDanielOC@users.noreply.github.com>
2024-09-20 09:29:13 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,44 @@
+print("Aktibidad nuebe klases v2")
+print("Olave Cruz Mat: 21308051280458")
+#Zona de class  
+class Operadores0458:
+        #Zona de funciones
+        def suma0458(self,E,N):
+            s0458=E+N
+            print(f"La suma de {E} + {N} = {s0458}")
+        def Resta0458(self,A,B):
+            r0458=A-B
+            print(f"La resta de {A} - {B} = {r0458}")
+        def Multiplicación0458(self,F,G):
+            m0458=F*G
+            print(f"La multiplicación de {F} * {G} = {m0458}")
+        def División0458(self,H,I):
+            d0458=H/I
+            print(f"La división de {H} / {I} = {d0458}")
+        def Exponente0458(self,J,K):
+            e0458=J**K
+            print(f"El exponente de {J} ** {K} = {e0458}")
+        def Módulo0458(self,L,M):
+            mo0458=L%M
+            print(f"El módulo de {L} % {M} = {mo0458}")
+        def Cociente0458(self,O,P):
+            c0458=O//P
+            print(f"El cociente de {O} // {P} = {c0458}")
+        
+# Creación del objeto
+Operazion=Operadores0458()
+#Zona donde uso objetos
+print("Función Suma")
+Operazion.suma0458(5,4)
+print("Función Resta")
+Operazion.Resta0458(5,4)
+print("Función Multiplicación")
+Operazion.Multiplicación0458(5,4)
+print("Función División")
+Operazion.División0458(5,4)
+print("Función Exponente")
+Operazion.Exponente0458(5,4)
+print("Función Módulo")
+Operazion.Módulo0458(5,4)
+print("Función Cociente")
+Operazion.Cociente0458(5,4)
